3296b55df2c7be245500f6cb3775380bc2f53d90
Damjan Pavlica <mudroljub@gmail.com>
2017-12-08 13:15:08 +0100

    refaktor

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,30 +1,30 @@
 import React from 'react';
 
-const filteri = {
-  autor: '',
-  tekst: ''
-}
+const Filters = props => {
+  const filteri = {
+    autor: '',
+    tekst: ''
+  }
 
-export default class Filters extends React.Component {
-
-  handleChange = event => {
+  const handleChange = event => {
     filteri[event.target.name] = event.target.value
-    this.props.filtriraj(filteri)
+    props.filtriraj(filteri)
   }
 
-  render() {
-    const sortirano = [...this.props.autori].sort((a, b) => a > b ? 1 : (b > a ? -1 : 0))
-    const options = sortirano.map((autor, i) =>
-      <option key={i} value={autor}>{autor}</option>
-    )
-    return (
-      <div>
-        <input name="tekst" onChange={this.handleChange} />
-        <select name="autor" onChange={this.handleChange}>
-          <option value=''>Svi autori</option>
-          {options}
-        </select>
-      </div>
-    )
-  }
+  const sortirano = [...props.autori].sort((a, b) => a > b ? 1 : (b > a ? -1 : 0))
+  const options = sortirano.map((autor, i) =>
+    <option key={i} value={autor}>{autor}</option>
+  )
+
+  return (
+    <div>
+      <input name="tekst" onChange={handleChange} />
+      <select name="autor" onChange={handleChange}>
+        <option value=''>Svi autori</option>
+        {options}
+      </select>
+    </div>
+  )
 }
+
+export default Filters
