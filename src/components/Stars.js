b4bf7721c075ec25776d4a5d25bdf53be29b1def
NemanjaDjo <ndjorovic1@gmai.com>
2018-02-04 13:19:19 +0100

    load, display and count the average ratings

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,10 +1,37 @@
 import React, {Component} from 'react'
 import ReactStars from 'react-stars'
 
-class Stars extends Component {
+
+class Stars extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = { 
+            edit: true,
+            rating: Number(this.props.rating)
+        }
+
+    }
+    
+
+    rate = (newRating) => {
+
+        const http = new XMLHttpRequest()
+        http.open('POST', 'https://baza-podataka.herokuapp.com/oceni-citat/')
+        http.setRequestHeader('Content-type', 'application/json')
+        http.onload = () => console.log(http.responseText)
+        http.send(JSON.stringify({'_id': this.props.id, 'novaOcena': newRating}))
+        
+        this.setState({ 
+            edit: false,
+            rating: newRating
+        })
+
+
+    }
     render(){
         return(
-            <ReactStars size={20} />
+            <ReactStars size={20} value={this.state.rating} onChange={this.rate} edit={this.state.edit} /> 
         )
     }
 }
