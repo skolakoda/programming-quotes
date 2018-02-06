d94acc05e4699402aba50296e96a058f1d99dbb2
mudroljub <mudroljub@gmail.com>
2018-02-07 00:37:10 +0100

    refaktorisana komponenta, obrisan css

--- src/components/AddQuote.jsx
+++ src/components/AddQuote.jsx
@@ -1,73 +1,72 @@
 import React from 'react'
-import './AddQuote.css'
 
 class AddQuote extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      autor: '',
-      quote: ''
-    }
-    this.addQuote = this.addQuote.bind(this);
-    this.handleInput = this.handleInput.bind(this);
-    this.deleteQuote = this.deleteQuote.bind(this);
-  }
+	constructor(props) {
+		super(props)
+		this.state = {}
+	}
 
-  handleInput(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
-    this.setState({
-      [name]: value
-    });
-  }
+	handleInput = event => {
+    const {name, value} = event.target
+		this.setState({
+			[name]: value
+		})
+	}
 
-  addQuote(e) {
-    e.preventDefault();
+	addQuote = e => {
+    e.preventDefault()
     fetch("https://baza-podataka.herokuapp.com/dodaj-citat/", {
-      method: 'post',
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        autor: this.state.autor,
-        sr: this.state.quote
-      })
-    })
-    alert(`Quote by: "${this.state.autor}" has been updated to quote base.
-Thank you for updating!`);
+			method: 'post',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify(this.state)
+		})
+    .then(response => response.text())
+    .then(response => console.log(response))
   }
 
-  deleteQuote(e) {
-    e.preventDefault();
-    fetch("https://baza-podataka.herokuapp.com/obrisi-citat/", {
-      method: 'delete',
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        _id: "kjdjkjawkj"
-      })
-    })
-    alert(`Deleted`);
-  }
+	deleteQuote = e => {
+		e.preventDefault()
+		fetch("https://baza-podataka.herokuapp.com/obrisi-citat/", {
+			method: 'delete',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify({
+				_id: "kjdjkjawkj"
+			})
+		})
+    .then(response => response.text())
+    .then(response => console.log(response))
+	}
 
   render() {
-    return (
-      <div>
-        <h1>Add Quote</h1>
-        <form className="center" onSubmit={this.addQuote} >
-          <label>Author Name:</label>
-          <input type="text" placeholder="Author.." name='autor' onChange={this.handleInput} />
-          <textarea name="quote" cols="35" rows="10" style={{ resize: 'none' }} onChange={this.handleInput} placeholder="Quote text.."></textarea>
-          <input id="submit" type="submit" value="Add Quote"></input>
-          <button onClick={this.deleteQuote}>Delete quote</button>
-        </form>
-      </div>
-    );
+   return (
+    <div>
+      <h1>Add Quote</h1>
+      <form className="center" onSubmit={this.addQuote} >
+        <p>
+          <label>Author name (<small>like on en.wikipedia</small>) </label><br/>
+          <input name='autor' onChange={this.handleInput} required />
+        </p>
+        <p>
+          <label>English version</label><br/>
+          <textarea name="en" cols="60" rows="5" onChange={this.handleInput}></textarea>
+        </p>
+        <p>
+          <label>Serbian translation</label><br/>
+          <textarea name="sr" cols="60" rows="5" onChange={this.handleInput}></textarea>
+        </p>
+        <p>
+          <label>Source (<small>optional</small>): </label><br/>
+          <input name='izvor' onChange={this.handleInput} />
+        </p>
+        <p>
+          <small>* Author and at least one language is required.</small>
+        </p>
+        <input type="submit" value="Add Quote"></input>
+        {/* <button onClick={this.deleteQuote}>Delete quote</button> */}
+      </form>
+    </div>
+    )
   }
 }
 
-export default AddQuote;
+export default AddQuote
