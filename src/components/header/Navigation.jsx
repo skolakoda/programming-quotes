a84309c0a199317b3dd6cb54dde51579fee100c8
Damjan Pavlica <mudroljub@gmail.com>
2018-02-07 22:37:44 +0100

    format code

--- src/components/header/Navigation.jsx
+++ src/components/header/Navigation.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Github from './Github'
 
 const Navigation = ({language, changeLang}) => (
-    <header>
-        <Github repoUrl="https://github.com/skolakoda/programerski-citati" />
-        <h1>{language === 'en'
-                ? 'Programming quotes'
-                : 'Programerski citati'}</h1>
-        <button onClick={() => changeLang('sr')} className="lang-btn">SRB</button>
-        <button onClick={() => changeLang('en')} className="lang-btn">ENG</button>
-    </header>
+  <header>
+    <Github repoUrl="https://github.com/skolakoda/programerski-citati" />
+    <h1>{language === 'en'
+      ? 'Programming quotes'
+      : 'Programerski citati'}</h1>
+    <button onClick={() => changeLang('sr')} className="lang-btn">SRB</button>
+    <button onClick={() => changeLang('en')} className="lang-btn">ENG</button>
+  </header>
 )
 
 export default Navigation
