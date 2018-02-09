18316da3aa4ac3875d2794f38c1b9e2e84884391
Damjan Pavlica <dp@playerhunter.com>
2018-02-09 15:37:49 +0100

    small refactor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -32,32 +32,8 @@ class App extends Component {
       const currentQuotes = allQuotes.filter(x => Math.random() > .9)
       const authors = new Set(allQuotes.map(quote => quote.autor))
       this.setState(() => ({allQuotes, currentQuotes, authors}))
-      for (const author of authors) this.getImage(author)
+      for (const author of authors) this.fetchImage(author)
     })
-  }
-
-  getImage(author) {
-    fetch(`https://en.wikipedia.org/w/api.php?action=query&titles=${author}&prop=pageimages&format=json&pithumbsize=50&origin=*`)
-    .then(response => response.json())
-    .then(obj => {
-      const imgSrc = findProp(obj, 'source') || 'images/unknown.jpg'
-      const authorImages = new Map(this.state.authorImages).set(author, imgSrc)
-      this.setState(() => ({authorImages}))
-    })
-  }
-
-  setPhrase = event => {
-    this.setState({phrase:event.target.value}, this.filterQuotes)
-  }
-
-  setAuthor = chosenAuthor => {
-    fetch(`https://en.wikipedia.org/w/api.php?action=query&titles=${chosenAuthor}&prop=pageimages&format=json&pithumbsize=250&origin=*`)
-      .then(response => response.json())
-      .then(obj => {
-        const mainImage = findProp(obj,'source') || 'images/unknown.jpg'
-        this.setState({mainImage})
-      })
-    this.setState({chosenAuthor}, this.filterQuotes)
   }
 
   filterQuotes = () => {
@@ -68,6 +44,34 @@ class App extends Component {
       && quote[language].toLowerCase().includes(this.state.phrase.toLowerCase())
     )
     this.setState({currentQuotes})
+  }
+
+  fetchImage(author) {
+    fetch(`https://en.wikipedia.org/w/api.php?action=query&titles=${author}&prop=pageimages&format=json&pithumbsize=50&origin=*`)
+    .then(response => response.json())
+    .then(obj => {
+      const imgSrc = findProp(obj, 'source') || 'images/unknown.jpg'
+      const authorImages = new Map(this.state.authorImages).set(author, imgSrc)
+      this.setState({authorImages})
+    })
+  }
+
+  fetchMainImage(author) {
+    fetch(`https://en.wikipedia.org/w/api.php?action=query&titles=${author}&prop=pageimages&format=json&pithumbsize=250&origin=*`)
+    .then(response => response.json())
+    .then(obj => {
+      const mainImage = findProp(obj,'source') || 'images/unknown.jpg'
+      this.setState({mainImage})
+    })
+  }
+
+  setAuthor = chosenAuthor => {
+    this.fetchMainImage(chosenAuthor)
+    this.setState({chosenAuthor}, this.filterQuotes)
+  }
+
+  setPhrase = event => {
+    this.setState({phrase:event.target.value}, this.filterQuotes)
   }
 
   setLang = (language) => {
