2b14d7a6a3a9516c430521e0ed94f6ddd04453df
Sudimac <a.sudimac86@gmail.com>
2018-02-11 20:13:27 +0100

    add filter authors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
 import App from './components/App'
 import './index.css'
 
-render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-), document.getElementById('root'))
+render(
+  <Router>
+    <Route exact path='/' component={App} />
+  </Router>
+  , document.getElementById('root'))
+
 
 registerServiceWorker()
