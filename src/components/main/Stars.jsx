801cb084ed063f72dd726edf8d86e520546ed351
Damjan Pavlica <dp@playerhunter.com>
2018-02-12 17:55:11 +0100

    minor cleanup

--- src/components/main/Stars.jsx
+++ src/components/main/Stars.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import ReactStars from 'react-stars'
 import { vote } from '../../shared/helpers'
-import './stars.css'
+import './Stars.css'
 
 class Stars extends Component {
   constructor(props) {
@@ -14,52 +14,52 @@ class Stars extends Component {
   }
 
   onRating(newRating) {
-    let storage = JSON.parse(localStorage.getItem('programerskicitatiocene'))
+    const storage = JSON.parse(localStorage.getItem('programerskicitatiocene'))
     if(!storage) {
       vote(this.props.id, newRating)
         .then(res => {
           if(res === 'OK') {
-            let storageData = [
-                this.props.id
+            const storageData = [
+              this.props.id
             ]
             localStorage.setItem('programerskicitatiocene', JSON.stringify(storageData))
             this.setState({ rating: newRating})
           } else {
-            this.setState({ 
+            this.setState({
               error: true,
               errorMessage: 'Greska prilikom glasanja, pokusajte ponovo kasnije!'
             })
           }
-      })
+        })
     } else if(Array.isArray(storage) && storage.indexOf(this.props.id) < 0) {
-        vote(this.props.id, newRating)
-          .then(res => {
-            if(res === 'OK') {
-              let storageData = [
-                  ...storage,
-                  this.props.id
-              ]
-              localStorage.setItem('programerskicitatiocene', JSON.stringify(storageData))
-              this.setState({ rating: newRating})
+      vote(this.props.id, newRating)
+        .then(res => {
+          if(res === 'OK') {
+            const storageData = [
+              ...storage,
+              this.props.id
+            ]
+            localStorage.setItem('programerskicitatiocene', JSON.stringify(storageData))
+            this.setState({ rating: newRating})
           }
-      })
+        })
     } else {
       this.setState({
-          rating: Number(this.state.rating),
-          error: true,
-          errorMessage: 'You can vote just once!'
+        rating: Number(this.state.rating),
+        error: true,
+        errorMessage: 'You can vote just once!'
       })
     }
-}
+  }
 
-render(){
-  return(
-    <div>
-      <ReactStars size={20} value={this.state.rating} onChange={this.onRating.bind(this)}/>
-      {this.state.error && <p className="voting-error">{this.state.errorMessage}</p>}
-    </div>
-  )
-}
+  render() {
+    return(
+      <div>
+        <ReactStars size={20} value={this.state.rating} onChange={this.onRating.bind(this)}/>
+        {this.state.error && <p className="voting-error">{this.state.errorMessage}</p>}
+      </div>
+    )
+  }
   // constructor(props) {
   //   super(props)
   //   this.state = {
--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import registerServiceWorker from './registerServiceWorker'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './components/App'
 import './index.css'
 
 render(
   <Router>
-    <Route exact path='/' component={App} />
-  </Router>
-  , document.getElementById('root'))
-
-
-registerServiceWorker()
+    <App />
+  </Router>, document.getElementById('root'))
