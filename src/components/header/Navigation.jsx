4fa7fc232bc212876368506e59c93dd31e84db58
Damjan Pavlica <mudroljub@gmail.com>
2018-02-13 21:28:24 +0100

    naslov

--- src/components/header/Navigation.jsx
+++ src/components/header/Navigation.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Github from './Github'
 import {Link} from 'react-router-dom'
-import translate from '../../shared/translate'
 import './Navigation.css'
 
 const Navigation = ({setLang}) => (
   <header>
     <Github repoUrl="https://github.com/skolakoda/programerski-citati" />
-    <h1>{translate('PROGRAMMING_QUOTES')}</h1>
     <nav>
       <Link to="/">Home</Link>
       <Link to="/add-quote">Add quote</Link>
