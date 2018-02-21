45c778fc0e2adaef91e91575ff8ecf2ac10bfc61
Damjan Pavlica <mudroljub@gmail.com>
2018-02-21 22:28:15 +0100

    even more cleanup

--- src/components/main/Picture.jsx
+++ src/components/main/Picture.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {fetchImage} from '../../shared/helpers'
 import './Picture.css'
 
-const Picture = ({ author, imgSrc }) => {
-  if (!imgSrc) return ''
-  return (
-    <div className="thumbnail">
-      <h3>{author}</h3>
-      <img className="main-image" src={imgSrc} alt={author} />
-    </div>
-  )
+class Picture extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {image: ''}
+  }
+
+  componentDidMount() {
+    this.getImage(this.props.author)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.author !== nextProps.author)
+      this.getImage(nextProps.author)
+  }
+
+  getImage(author) {
+    this.setState({image: ''})
+    fetchImage(author, '250', src =>
+      this.setState({image: src || '/images/unknown.jpg'}))
+  }
+
+  render() {
+    const { author } = this.props
+    return (
+      <div className="thumbnail">
+        <h3>{author}</h3>
+        <img className="main-image" src={this.state.image} alt={author} />
+      </div>
+    )
+  }
 }
 export default Picture
--- src/routes/Author.jsx
+++ src/routes/Author.jsx
@@ -1,29 +1,8 @@
 import React, {Component} from 'react'
 import Quotes from '../components/main/Quotes'
 import Picture from '../components/main/Picture'
-import {fetchImage} from '../shared/helpers'
 
 class Author extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {image: ''}
-  }
-
-  componentDidMount() {
-    this.getImage(this.props)
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.match.params.name !== this.props.match.params.name)
-      this.getImage(nextProps)
-  }
-
-  getImage(props) {
-    this.setState({image: ''})
-    const author = props.match.params.name
-    fetchImage(author, '250', image => this.setState({image}))
-  }
-
   render() {
     const author = this.props.match.params.name
     const { language, allQuotes, password } = this.props
@@ -32,7 +11,7 @@ class Author extends Component {
     return (
       <main>
         <h1>{author}</h1>
-        <Picture imgSrc={this.state.image} author={author} />
+        <Picture author={author} />
         <Quotes language={language} currentQuotes={currentQuotes} password={password} />
       </main>
     )
