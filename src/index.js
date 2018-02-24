460740a39753046c033a5f552a4ad0956cbcded0
Damjan Pavlica <mudroljub@gmail.com>
2018-02-24 22:56:04 +0100

    change to hash route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
 
 import {basename} from './config/router'
