559fd8d08ddd924334f0f5210deb23ecd1e7afd1
Damjan Pavlica <mudroljub@gmail.com>
2018-02-24 22:18:46 +0100

    fix images routes

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import translate from '../shared/translate'
 import Quotes from '../components/main/Quotes'
+import preloader from '../images/preloader.gif'
 
 const numberOfQuotes = 10
 
@@ -14,7 +15,7 @@ const Home = ({ language, allQuotes, phrase, password }) => {
       <h1>{translate('PROGRAMMING_QUOTES')}</h1>
       {allQuotes.length
         ? <Quotes language={language} currentQuotes={currentQuotes} password={password} />
-        : <img src="/images/preloader.gif" alt="loading..." />
+        : <img src={preloader} alt="loading..." />
       }
     </main>
   )
