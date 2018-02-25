e653e3267e3fbeccea46063111c7f74dcc0cf025
Damjan Pavlica <mudroljub@gmail.com>
2018-02-25 23:33:26 +0100

    add delete procedure and popup

--- src/components/main/Quote.jsx
+++ src/components/main/Quote.jsx
@@ -1,42 +1,66 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import Stars from './Stars'
+import MessagePopup from './MessagePopup'
+import translate from '../../shared/translate'
 import './Quote.css'
 
-const Quote = ({author, content, rating, id, password}) => {
-  const quoteLink = `/quote/${id}`
-  const editLink = `/edit-quote/${id}`
-  const authorLink = `/author/${author}`
-  const wikiUrl = `https://en.wikipedia.org/wiki/${author}`
-  let shouldDelete = false
-
-  const tryDelete = () => {
-    if (shouldDelete)
-      deleteQuote()
-    shouldDelete = true
+class Quote extends Component {
+  constructor() {
+    super()
+    this.state = {
+      shouldDelete: false,
+      response: ''
+    }
   }
 
-  const deleteQuote = () => {
+  tryDelete = () => {
+    if (this.state.shouldDelete)
+      this.deleteQuote()
+    this.setState({shouldDelete: true})
+  }
+
+  deleteQuote = () => {
     fetch('https://baza-podataka.herokuapp.com/obrisi-citat/', {
       method: 'delete',
       headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({_id: id, password})
+      body: JSON.stringify({_id: this.props.id, password: this.props.password})
     })
       .then(response => response.text())
-      .then(response => response === 'ok' && window.location.reload())
+      .then(response => this.setState({response: translate(response)}))
   }
 
-  return (
-    <blockquote>
-      <Link to={quoteLink} className="no-link"><i>{content}</i></Link>&nbsp;
-      { password && <Link to={editLink}><span className="edit-icon">&#9998;</span></Link> }
-      { password && <span onClick={tryDelete} className="pointer">&#10005;</span> }
-      <br/>
+  closePopup = () => {
+    this.setState({ response: '' })
+    window.location.reload() // TODO: remove quote from allQuotes
+  }
 
-      <small> — <Link to={authorLink}>{author}</Link> <small>(<a href={wikiUrl} target="_blank">wiki</a>)</small> </small>
-      <Stars rating={rating} id={id} />
-    </blockquote>
-  )
+  render() {
+    const {author, content, rating, id, password} = this.props
+    const quoteLink = `/quote/${id}`
+    const editLink = `/edit-quote/${id}`
+    const authorLink = `/author/${author}`
+    const wikiUrl = `https://en.wikipedia.org/wiki/${author}`
+    const deleteStyle = `pointer ${this.state.shouldDelete ? 'red' : ''}`
+
+    return (
+      <blockquote>
+        <Link to={quoteLink} className="no-link"><i>{content}</i></Link>&nbsp;
+        { password &&
+          <span>
+            <Link to={editLink}><span className="edit-icon">&#9998;</span></Link>
+            <span onClick={this.tryDelete} className={deleteStyle}>&#10005;</span>
+          </span>
+        }
+        <br/>
+
+        <small> — <Link to={authorLink}>{author}</Link> <small>(<a href={wikiUrl} target="_blank">wiki</a>)</small> </small>
+        <Stars rating={rating} id={id} />
+
+        {this.state.response && <MessagePopup message={this.state.response} closePopup={this.closePopup} />}
+      </blockquote>
+    )
+  }
 }
 
 export default Quote
