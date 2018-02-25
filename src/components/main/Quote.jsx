767e0d3ffd8e1dee243ce6bd03819d257a694e90
Damjan Pavlica <mudroljub@gmail.com>
2018-02-25 23:09:24 +0100

    add delete quote

--- src/components/main/Quote.jsx
+++ src/components/main/Quote.jsx
@@ -8,12 +8,31 @@ const Quote = ({author, content, rating, id, password}) => {
   const editLink = `/edit-quote/${id}`
   const authorLink = `/author/${author}`
   const wikiUrl = `https://en.wikipedia.org/wiki/${author}`
+  let shouldDelete = false
+
+  const tryDelete = () => {
+    if (shouldDelete)
+      deleteQuote()
+    shouldDelete = true
+  }
+
+  const deleteQuote = () => {
+    fetch('https://baza-podataka.herokuapp.com/obrisi-citat/', {
+      method: 'delete',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({_id: id, password})
+    })
+      .then(response => response.text())
+      .then(response => response === 'ok' && window.location.reload())
+  }
 
   return (
     <blockquote>
       <Link to={quoteLink} className="no-link"><i>{content}</i></Link>&nbsp;
       { password && <Link to={editLink}><span className="edit-icon">&#9998;</span></Link> }
+      { password && <span onClick={tryDelete} className="pointer">&#10005;</span> }
       <br/>
+
       <small> — <Link to={authorLink}>{author}</Link> <small>(<a href={wikiUrl} target="_blank">wiki</a>)</small> </small>
       <Stars rating={rating} id={id} />
     </blockquote>
