89f42b5d63c15d1f2b4be748fa0968b7f3b246f9
mudroljub <mudroljub@gmail.com>
2018-03-04 15:54:39 +0100

    build random quote comp

--- src/routes/RandomQuote.jsx
+++ src/routes/RandomQuote.jsx
@@ -1,16 +1,25 @@
-import React, {Component} from 'react'
-// import Quote from './../components/main/Quote'
+import React, { Component } from 'react'
+import Quote from './../components/main/Quote'
 
 export default class RandomQuote extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {quote: null}
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const allQuotes = nextProps.allQuotes
+    this.setState({ quote: allQuotes[Math.floor(Math.random() * allQuotes.length)] })
   }
 
   render() {
+    const { language, password } = this.props
+    if (!this.state.quote || !this.state.quote[language]) return null
+
     return (
       <main>
         <h1>Quote of the day</h1>
+        <Quote language={language} quote={this.state.quote} id={this.state.quote._id} password={password} cssClass="big-quote" />
       </main>
     )
   }
