53f4ac52099064669244ef97f8d7d67d0b167fa4
Damjan Pavlica <mudroljub@gmail.com>
2018-03-05 22:19:28 +0100

    fix image loading

--- src/components/main/AuthorImage.jsx
+++ src/components/main/AuthorImage.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, {Component} from 'react'
 import unknownImage from '../../assets/images/unknown.jpg'
 
 const responsiveBreakpoint = 800
 
-const AuthorImage = ({author, allImages, showUnknown}) => {
-  const src = allImages.get(author)
-  const imgWidth = window.innerWidth < responsiveBreakpoint ? window.innerWidth : 250
-  const unknown = showUnknown ? unknownImage : ''
-  const newSrc = src ? src.replace(/\d+px/, `${imgWidth}px`) : unknown
-  if (!newSrc) return null
+class AuthorImage extends Component {
+  constructor() {
+    super()
+    this.state = {loaded: false}
+  }
+  
+  componentDidMount() {
+    this.setImage(this.props)
+  }
 
-  return (
-    <img className="main-image" src={newSrc} alt={author} />
-  )
+  componentWillReceiveProps(nextProps) {
+    this.setImage(nextProps)
+  }
+
+  setImage(props) {
+    this.setState({loaded: false})
+    const {author, allImages, showUnknown} = props
+    const src = allImages.get(author)
+    const imgWidth = window.innerWidth < responsiveBreakpoint ? window.innerWidth : 250
+    const unknown = showUnknown ? unknownImage : ''
+    const newSrc = src ? src.replace(/\d+px/, `${imgWidth}px`) : unknown
+    this.setState({src: newSrc})
+  }
+
+  render() {
+    return (
+      <img 
+        className="main-image" 
+        src={this.state.src} 
+        style={this.state.loaded ? {} : {display: 'none'}}
+        onLoad={() => this.setState({loaded: true})}
+        alt={'author'}
+      />
+    )
+  }
 }
+
 export default AuthorImage
