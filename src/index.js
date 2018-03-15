03cce091cf44331bd6dd5539dfd68926ce624561
Damjan Pavlica <dp@playerhunter.com>
2018-03-15 13:58:46 +0100

    test router (2)

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import { render } from 'react-dom'
 import { HashRouter as Router } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
 
-import {basename} from './config/router'
+// import {basename} from './config/router'
 import App from './components/App'
 import './index.css'
 
 render(
-  <Router basename={basename}>
+  <Router basename={''}>
     <App />
   </Router>, document.getElementById('root'))
 
