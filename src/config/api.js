5e90607b47b86d3c98a66cfddfac70b295f00b38
Damjan Pavlica <mudroljub@gmail.com>
2019-06-15 01:14:12 +0200

    update api url

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 export const domain = process.env.REACT_APP_ENV === 'development'
   ? 'http://localhost:5000'
-  : 'https://pqapi.herokuapp.com'
+  : 'https://programming-quotes-api.herokuapp.com'
 
 export const API = {
   create: `${domain}/quotes/`,
