e391a4d203868efe74a966437c0602149a2683bc
Damjan Pavlica <damjan.pavlica@origo.is>
2019-12-31 10:53:29 +0100

    m

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect, useDispatch } from 'react-redux'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 import translate from '../../shared/translate'
 import {setLanguage} from '../../store/actions'
