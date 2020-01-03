e366d3715bb1b432ff0f97b1d039fa1042503997
Damjan Pavlica <damjan.pavlica@origo.is>
2020-01-03 13:09:00 +0100

    useAuthorName

--- src/routes/Author.jsx
+++ src/routes/Author.jsx
@@ -4,21 +4,21 @@ import {useSelector} from 'react-redux'
 import Quotes from '../components/main/Quotes'
 import AuthorImage from '../components/main/AuthorImage'
 import AuthorInfo from '../components/main/AuthorInfo'
-
+import {useAuthorName} from '../store/actions'
 import './Author.css'
 
 const Author = ({match}) => {
   const {filteredQuotes} = useSelector(state => state)
+  const getName = useAuthorName()
 
   const author = match.params.name.replace(/_/g, ' ')
-
   const filtered = filteredQuotes.filter(q => q.author === author)
 
   return (
     <main>
-      <h1>{author}</h1>
+      <h1>{getName(author)}</h1>
       <div className="thumbnail">
-        <h3 className="hide-sm">{author}</h3>
+        <h3 className="hide-sm">{getName(author)}</h3>
         <AuthorImage author={author} />
         <AuthorInfo author={author} />
       </div>
